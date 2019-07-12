925dff20a6e1d3beef48770fda5dbd74a3454b98
Robert Baxter <baxterrp@gmail.com>
2019-07-11 21:33:55 -0400

    Initial Commit

---
+++ AHBC_July_Lab2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AHBC_July_Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
